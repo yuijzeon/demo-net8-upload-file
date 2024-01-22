83a3a13416fce3d9c9b581fe8e5c0c16f8e14e15
yuijzeon <dingyujen@gmail.com>
2024-01-22 09:27:39 +0800

    initial project

---
+++ UploadFile/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFile;
+
+public class TestController : ControllerBase
+{
+    [HttpPut("/test")]
+    public OkResult UploadFile([FromForm] IFormFile file)
+    {
+        Console.WriteLine(file.FileName);
+        return Ok();
+    }
+}
